981baf3110bea0cc7e647d570f53caf0501ecd6f
Lungeanu Marius <34165461+lngmarius@users.noreply.github.com>
2022-11-05 19:58:23 +0200

    Add files via upload

---
+++ password_generator/src/main/java/com/mycompany/password_generator/Password_generator.java
@@ -0,0 +1,51 @@
+package com.mycompany.password_generator;
+
+
+
+public class Password_generator {
+
+    public String[] chars=new String[100];
+    
+    Password_generator()
+    {
+        int j=0;
+        for(int i=33;i<127;i++)
+        { this.chars[j]=Character.toString((char)i);
+        j++;
+        }
+        
+    }
+
+    void Afiseaza()
+    {
+    for(int i=0;i<93;i++)
+        System.out.println(chars[i]);
+    }
+    
+    public String getChar(int n)
+    {
+    return this.chars[n];
+    }
+    
+   String Random(int n)
+   {
+   
+   String s=new String(); 
+   for(int i=0;i<n;i++)
+     s=s+getChar((int)(Math.random()*93)+1);
+   
+   return s;
+   }
+    
+    
+    
+    
+    public static void main(String[] args) {
+    
+    Password_generator vect = new Password_generator();
+    vect.Afiseaza();
+    System.out.println("------------");
+    
+     System.out.println((vect.Random(32)));
+    }
+}
